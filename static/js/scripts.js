a2137dc13c1f92eb4ddaf650a47df2a7840a9e1a
tea-eclair <148536484+tea-eclair@users.noreply.github.com>
2023-11-13 17:03:17 +0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -35,3 +35,35 @@ function InsertionSort(A)       // A - массив, который нужно
      }                    
     return A;    // На выходе сортированный по возрастанию массив A.
 }
+
+
+function ShellSort(A)
+{
+    var n = A.length, i = Math.floor(n/2);
+    while (i > 0)
+     { for (var j = 0; j < n; j++)
+        { var k = j, t = A[j];
+          while (k >= i && A[k-i] > t)
+           { A[k] = A[k-i]; k -= i; }
+          A[k] = t;
+        }
+      i = (i==2) ? 1 : Math.floor(i*5/11);
+     }
+    return A;
+}
+
+function SimpleCountingSort(A)
+{   
+    var n = A.length, Count = [], B = [];
+    for (var i = 0; i < n; i++) Count[ i ] = 0;
+    for (var i = 0; i < n-1; i++)
+     { for (var j = i+1; j < n; j++)
+        { if (A[ i ] < A[j]) Count[j]++;
+          else Count[ i ]++;
+        }
+     }
+    for (var i = 0; i < n; i++) B[Count[ i ]] = A[ i ];
+    return B;
+}
+
+
