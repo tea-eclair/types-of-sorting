46758897f9c50e31d52240e3c1c498a28eb4950d
tea-eclair <148536484+tea-eclair@users.noreply.github.com>
2023-11-13 16:45:05 +0500

    Update server.py

--- server.py
+++ server.py
@@ -16,8 +16,10 @@ def sort(algorithm):
         sorted_array = selection_sort(array_to_sort)
     elif algorithm == 'insertion':
         sorted_array = insertion_sort(array_to_sort)
+    elif algorithm == 'shell':
+        sorted_array = shell_sort(array_to_sort)
     else:
-        sorted_array = bubble_sort(array_to_sort)
+        sorted_array = simple_counting_sort(array_to_sort)
 
     return jsonify(sorted_array)
 
@@ -31,6 +33,14 @@ def selection_sort(arr):
 
 def insertion_sort(arr):
     # Реализация сортировки вставками
+    return arr
+
+def shell_sort(arr):
+    # Реализация сортировки Шелла
+    return arr
+
+def simple_counting_sort(arr):
+    # Реализация сортировки подсчетом
     return arr
 
 if __name__ == '__main__':
