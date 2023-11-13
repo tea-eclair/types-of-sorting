b40887f35db08b5945b731a97a692312afdaaed1
tea-eclair <148536484+tea-eclair@users.noreply.github.com>
2023-11-13 16:24:41 +0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/sort/<algorithm>')
+def sort(algorithm):
+    array_to_sort = [64, 25, 12, 22, 11]
+
+    if algorithm == 'bubble':
+        sorted_array = bubble_sort(array_to_sort)
+    elif algorithm == 'selection':
+        sorted_array = selection_sort(array_to_sort)
+    elif algorithm == 'insertion':
+        sorted_array = insertion_sort(array_to_sort)
+    else:
+        sorted_array = bubble_sort(array_to_sort)
+
+    return jsonify(sorted_array)
+
+def bubble_sort(arr):
+    # Реализация сортировки пузырьком
+    return arr
+
+def selection_sort(arr):
+    # Реализация сортировки выбором
+    return arr
+
+def insertion_sort(arr):
+    # Реализация сортировки вставками
+    return arr
+
+if __name__ == '__main__':
+    app.run(debug=True)
